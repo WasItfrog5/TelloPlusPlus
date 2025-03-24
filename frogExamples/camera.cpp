06e4f0020f21106bf5ba112d1b3b1304c64a4389
WasItfrog5 <markomiloszivkovic@gmail.com>
2025-03-24 16:38:21 +0100

    new example (camera)

---
+++ frogExamples/camera.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/opencv.hpp>
+#include "Tello++1.cpp"
+
+int main() {
+    fr::TelloSDK13 tello;
+    tello.connect();
+    tello.streamon();
+    cv::VideoCapture cap("udp://192.168.10.1:11111");
+    cv::Mat img;
+    while(true) {
+        cap.read(img);
+        cv::imshow("Tello Camera", img);
+        if(cv::waitKey(1) == 27) {
+            break;
+        }
+    }
+    tello.close();
+}
